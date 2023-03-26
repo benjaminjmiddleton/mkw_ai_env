548b7adfaaa791e36b9a504b2c3f7b829dd8ab85
Benjamin Middleton <bjamesmiddleton@gmail.com>
2023-03-26 10:17:28 -0400

    cleanup

--- Rainbow/env.py
+++ Rainbow/env.py
@@ -3,8 +3,6 @@ from collections import deque
 import random
 import socket
 import json
-import atari_py
-import cv2
 import torch
 
 DRIFT_LEFT = -2
@@ -17,17 +15,6 @@ DRIFT_FORWARD = 3
 class Env():
   def __init__(self, args):
     self.device = args.device
-    # self.ale = atari_py.ALEInterface()
-    # self.ale.setInt('random_seed', args.seed)
-    # self.ale.setInt('max_num_frames_per_episode', args.max_episode_length)
-    # self.ale.setFloat('repeat_action_probability', 0)  # Disable sticky actions
-    # self.ale.setInt('frame_skip', 0)
-    # self.ale.setBool('color_averaging', False)
-    # self.ale.loadROM(atari_py.get_game_path(args.game))  # ROM loading must be done after setting options
-    # actions = self.ale.getMinimalActionSet() #[ 0  1  3  4 11 12]
-    # self.actions = dict([i, e] for i, e in zip(range(len(actions)), actions)) # {0: 0, 1: 1, 2: 3, 3: 4, 4: 11, 5: 12}
-    # self.lives = 0  # Life counter (used in DeepMind training)
-    # self.life_termination = False  # Used to check if resetting only from loss of life
     self.actions = {0: DRIFT_LEFT, 1: WHEELIE_LEFT, 2: WHEELIE_FORWARD, 3: WHEELIE_RIGHT, 4: DRIFT_RIGHT, 5: DRIFT_FORWARD}
     self.window = args.history_length  # Number of frames to concatenate
     self.state_buffer = deque([], maxlen=args.history_length)
@@ -38,10 +25,8 @@ class Env():
     sock.bind((host, port))
     sock.listen(1)
     self.client_socket, _ = sock.accept()
-    # self.training = True  # Consistent with model training mode
 
   def _get_state(self):
-    # state = cv2.resize(self.ale.getScreenGrayscale(), (84, 84), interpolation=cv2.INTER_LINEAR)
     message = json.loads(self.client_socket.recv(131072).decode('utf-8'))
     observation = message[0]
     reward = message[1]
@@ -55,67 +40,19 @@ class Env():
 
   def reset(self):
     self.client_socket.send( ( json.dumps( (WHEELIE_FORWARD, True) ).encode("utf-8") ) )
-    # if self.life_termination:
-    #   self.life_termination = False  # Reset flag
-    #   self.ale.act(0)  # Use a no-op after loss of life
-    # else:
-    # Reset internals
     self._reset_buffer()
-    # self.ale.reset_game()
-    # Perform up to 30 random no-ops before starting
-    # for _ in range(random.randrange(30)):
-    #   observation, _, _ = self._get_state()
-    #   self.client_socket.send( ( json.dumps( (WHEELIE_FORWARD, False) ).encode("utf-8") ) )
-      # self.ale.act(0)  # Assumes raw action 0 is always no-op
-      # if self.ale.game_over():
-      #   self.ale.reset_game()
-    # Process and return "initial" state
     observation, _, _, _ = self._get_state()
     self.state_buffer.append(observation)
-    # self.lives = self.ale.lives()
     return torch.stack(list(self.state_buffer), 0)
 
   def step(self, action):
-    # Repeat action 4 times, max pool over last 2 frames
-    # frame_buffer = torch.zeros(2, 84, 84, device=self.device)
-    # reward, done = 0, False
-    # for t in range(4):
-    #   reward += self.ale.act(self.actions.get(action))
-    #   if t == 2:
-    #     frame_buffer[0] = self._get_state()
-    #   elif t == 3:
-    #     frame_buffer[1] = self._get_state()
-    #   done = self.ale.game_over()
-    #   if done:
-    #     break
-    # observation = frame_buffer.max(0)[0]
     self.client_socket.send( ( json.dumps( (self.actions[action], False) ).encode("utf-8") ) )
     observation, reward, done, frame = self._get_state() # json.loads(self.client_socket.recv(131072).decode('utf-8'))
     self.state_buffer.append(observation)
-    # Detect loss of life as terminal in training mode
-    # if self.training:
-    #   lives = self.ale.lives()
-    #   if lives < self.lives and lives > 0:  # Lives > 0 for Q*bert
-    #     self.life_termination = not done  # Only set flag when not truly done
-    #     done = True
-    #   self.lives = lives
-    # Return state, reward, done
     return torch.stack(list(self.state_buffer), 0), reward, done, frame
-
-  # Uses loss of life as terminal signal
-  # def train(self):
-  #   self.training = True
-
-  # Uses standard terminal signal
-  # def eval(self):
-  #   self.training = False
 
   def action_space(self):
     return len(self.actions)
-
-  # def render(self):
-  #   cv2.imshow('screen', self.ale.getScreenRGB()[:, :, ::-1])
-  #   cv2.waitKey(1)
 
   def close(self):
     self.client_socket.close()
--- Rainbow/main.py
+++ Rainbow/main.py
@@ -14,7 +14,6 @@ from tqdm import trange
 from agent import Agent
 from env import Env
 from memory import ReplayMemory
-# from test import test
 
 
 # Note that hyperparameters may originally be reported in ATARI game frames instead of agent steps
@@ -22,9 +21,7 @@ parser = argparse.ArgumentParser(description='Rainbow')
 parser.add_argument('--id', type=str, default='default', help='Experiment ID')
 parser.add_argument('--seed', type=int, default=111, help='Random seed')
 parser.add_argument('--disable-cuda', action='store_true', help='Disable CUDA')
-# parser.add_argument('--game', type=str, default='space_invaders', choices=atari_py.list_games(), help='ATARI game')
 parser.add_argument('--T-max', type=int, default=int(50e6), metavar='STEPS', help='Number of training steps (4x number of frames)')
-# parser.add_argument('--max-episode-length', type=int, default=int(108e3), metavar='LENGTH', help='Max episode length in game frames (0 to disable)')
 parser.add_argument('--history-length', type=int, default=4, metavar='T', help='Number of consecutive states processed')
 parser.add_argument('--architecture', type=str, default='canonical', choices=['canonical', 'data-efficient'], metavar='ARCH', help='Network architecture')
 parser.add_argument('--hidden-size', type=int, default=512, metavar='SIZE', help='Network hidden size')
@@ -40,18 +37,11 @@ parser.add_argument('--priority-weight', type=float, default=0.4, metavar='β', 
 parser.add_argument('--multi-step', type=int, default=3, metavar='n', help='Number of steps for multi-step return')
 parser.add_argument('--discount', type=float, default=0.99, metavar='γ', help='Discount factor')
 parser.add_argument('--target-update', type=int, default=int(800), metavar='τ', help='Number of steps after which to update target network')
-# parser.add_argument('--reward-clip', type=int, default=1, metavar='VALUE', help='Reward clipping (0 to disable)')
 parser.add_argument('--learning-rate', type=float, default=0.0000625, metavar='η', help='Learning rate')
 parser.add_argument('--adam-eps', type=float, default=1.5e-4, metavar='ε', help='Adam epsilon')
 parser.add_argument('--batch-size', type=int, default=256, metavar='SIZE', help='Batch size')
 parser.add_argument('--norm-clip', type=float, default=10, metavar='NORM', help='Max L2 norm for gradient clipping')
 parser.add_argument('--learn-start', type=int, default=int(2000), metavar='STEPS', help='Number of steps before starting training')
-# parser.add_argument('--evaluate', action='store_true', help='Evaluate only')
-# parser.add_argument('--evaluation-interval', type=int, default=100000, metavar='STEPS', help='Number of training steps between evaluations')
-# parser.add_argument('--evaluation-episodes', type=int, default=10, metavar='N', help='Number of evaluation episodes to average over')
-# TODO: Note that DeepMind's evaluation method is running the latest agent for 500K frames ever every 1M steps
-# parser.add_argument('--evaluation-size', type=int, default=500, metavar='N', help='Number of transitions to use for validating Q')
-# parser.add_argument('--render', action='store_true', help='Display screen (testing only)')
 parser.add_argument('--enable-cudnn', action='store_true', help='Enable cuDNN (faster but nondeterministic)')
 parser.add_argument('--checkpoint-interval', type=int, default=0, help='How often to checkpoint the model, defaults to 0 (never checkpoint)')
 parser.add_argument('--memory', help='Path to save/load the memory from')
@@ -123,27 +113,10 @@ else:
 priority_weight_increase = (1 - args.priority_weight) / (args.T_max - args.learn_start)
 
 
-# Construct validation memory
-# val_mem = ReplayMemory(args, args.evaluation_size)
-# T, done = 0, True
-# while T < args.evaluation_size:
-#   if done:
-#     state = env.reset()
-
-#   next_state, _, done = env.step(np.random.randint(0, action_space))
-#   val_mem.append(state, -1, 0.0, done)
-#   state = next_state
-#   T += 1
-
-# if args.evaluate:
-#   dqn.eval()  # Set DQN (online network) to evaluation mode
-#   avg_reward, avg_Q = test(args, 0, dqn, val_mem, metrics, results_dir, evaluate=True)  # Test
-#   print('Avg. reward: ' + str(avg_reward) + ' | Avg. Q: ' + str(avg_Q))
-# else:
 # Training loop
 dqn.train()
 done = True
-for T in range(1, args.T_max + 1):
+for T in trange(1, args.T_max + 1):
   if done:
     state = env.reset()
 
@@ -152,10 +125,6 @@ for T in range(1, args.T_max + 1):
 
   action, val = dqn.act(state)  # Choose an action greedily (with noisy weights)
   next_state, reward, done, frame = env.step(action)  # Step
-  if frame % 56 == 0:
-    print(T, val)
-  # if args.reward_clip > 0:
-  #   reward = max(min(reward, args.reward_clip), -args.reward_clip)  # Clip rewards
   mem.append(state, action, reward, done)  # Append transition to memory
 
   # Train and test
@@ -165,23 +134,15 @@ for T in range(1, args.T_max + 1):
     if T % args.replay_frequency == 0:
       dqn.learn(mem)  # Train with n-step distributional double-Q learning
 
-    # if T % args.evaluation_interval == 0:
-    #   dqn.eval()  # Set DQN (online network) to evaluation mode
-    #   avg_reward, avg_Q = test(args, T, dqn, val_mem, metrics, results_dir)  # Test
-    #   log('T = ' + str(T) + ' / ' + str(args.T_max) + ' | Avg. reward: ' + str(avg_reward) + ' | Avg. Q: ' + str(avg_Q))
-    #   dqn.train()  # Set DQN (online network) back to training mode
-
-    #   # If memory path provided, save it
-    #   if args.memory is not None:
-    #     save_memory(mem, args.memory, args.disable_bzip_memory)
-
     # Update target network
     if T % args.target_update == 0:
       dqn.update_target_net()
 
     # Checkpoint the network
     if (args.checkpoint_interval != 0) and (T % args.checkpoint_interval == 0):
-      dqn.save(results_dir, 'checkpoint.pth')
+      dqn.save(results_dir, 'checkpoint'+str(T)+'.pth')
+      if args.memory is not None:
+        save_memory(mem, args.memory[:-4]+str(T)+args.memory[-4:], args.disable_bzip_memory)
 
   state = next_state
 
